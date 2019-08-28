e154f6fcb87fc1b1356c5de9eae56a3e3713ecf9
unknown <kevin0978159517@gmail.com>
2019-08-28 23:11:55 +0800

    DenseNet  class

--- layers.py
+++ layers.py
@@ -1,28 +1,75 @@
 import tensorflow as tf
 
-# 權重
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.1)
-    return tf.Variable(initial)
 
-# bias
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape = shape)
-    return tf.Variable(initial)
+class DenseNet():
 
-# 傳統的2D conv
-def conv2d(x, W, stride_num):
-    return tf.nn.conv2d(x, W, strides=[1, stride_num, stride_num, 1], padding='SAME')
-
-# maxpooling
-def max_pool(x, ksize_num,  stride_num):
-    return tf.nn.max_pool(x, ksize=[1, ksize_num, ksize_num, 1], strides=[1, stride_num, stride_num, 1], padding='SAME')
-
-# averagepooling
-def average_pool(x, ksize_num, stride_num):
-    return tf.nn.avg_pool(x, ksize=[1, ksize_num, ksize_num, 1], strides=[1, stride_num, stride_num, 1], padding="SAME")
-
-# DenseBlock
-def DenseBlock():
+    # 代表宣告時會自動執行的函式，是宣告類別的起手式
+    def __init__(self,       
+                num_classes,  # number of classes
+                growth_rate, 
+                bc_mode, 
+                block_config, 
+                reduction, 
+                dropout_rate, 
+                weight_decay, 
+                nesterov_momentum):
+        
+        self.num_classes = num_classes
+        self.growth_rate = growth_rate
+        self.bc_mode = bc_mode
+        self.block_config = block_config
+        self.reduction = reduction
+        self.dropout_rate = dropout_rate
+        self.weight_decay = weight_decay
+        self.nesterov_momentum = nesterov_momentum
     
-# batchnormalization
+    # 權重初始化
+    def weight_variable(self, 
+                        shape):
+
+        initial = tf.truncated_normal(shape, stddev = 0.1)
+        return tf.Variable(initial)
+
+    # bias 初始化
+    def bias_variable(self, 
+                        shape):
+
+        initial = tf.constant(0.1, shape = shape)
+        return tf.Variable(inital)
+
+    # 傳統的 2D conv
+    def conv2d(self, 
+                x, 
+                W, 
+                stride_num):
+
+        return tf.nn.conv2d(x, W, strides=[1, stride_num, stride_num, 1], padding = 'SAME')
+
+    # Maxpooling 
+    def max_pool(self, 
+                x, 
+                ksize_num, 
+                stride_num):
+
+        return tf.nn.max_pool(x, ksize = [1, ksize_num, ksize_num, 1], stides = [1, stride_num, stride_num, 1], padding = 'SAME')
+
+    # Average Pooling
+    def average_pool(self, 
+                        x, 
+                        ksize_num, 
+                        stride_num):
+
+        return tf.nn.avg_pool(x, ksize = [1, ksize_num, ksize_num, 1], strides = [1, stride_num, stride])
+
+    # Batch Normalization
+    def batch_norm(self, 
+                    x, 
+                    training):
+
+        return tf.keras.layers.BatchNormalization(x, training = training)
+
+    # Dropout
+    def dropout():
+
+
+
